d16b1394bcf1a49788035aa8afbabefcd51861d7
tpham812 <tpham812@localhost>
2014-05-04 12:10:39 -0400

    New

--- photoalbum58-android/gen/cs213/photoAlbum/android/R.java
+++ photoalbum58-android/gen/cs213/photoAlbum/android/R.java
@@ -1080,6 +1080,9 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
+        public static final int Apply=0x7f050040;
+        public static final int Cancel=0x7f05003f;
+        public static final int TextField=0x7f05003d;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,11 +1094,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050040;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003e;
+        public static final int button1=0x7f050042;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1113,7 +1116,7 @@ containing a value of this type.
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
-        public static final int listView1=0x7f05003d;
+        public static final int listView1=0x7f050041;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
@@ -1139,7 +1142,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003f;
+        public static final int textView1=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1196,16 +1199,17 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int activity_second=0x7f030019;
+        public static final int activity_edit_album=0x7f030018;
+        public static final int activity_main=0x7f030019;
         public static final int color_list=0x7f03001a;
-        public static final int fragment_main=0x7f03001b;
-        public static final int fragment_second=0x7f03001c;
+        public static final int fragment_edit_album=0x7f03001b;
+        public static final int fragment_main=0x7f03001c;
         public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
-        public static final int second=0x7f0c0001;
+        public static final int edit_album=0x7f0c0000;
+        public static final int main=0x7f0c0001;
+        public static final int second=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1251,6 +1255,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_edit_album=0x7f0a0011;
         public static final int title_activity_second=0x7f0a0010;
     }
     public static final class style {
