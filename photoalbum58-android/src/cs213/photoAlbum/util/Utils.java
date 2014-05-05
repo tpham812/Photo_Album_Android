70b04d4653574d7d2baba881a57df696fa34482b
dbas <dbas@localhost>
2014-05-05 07:24:43 -0400

    Fixed photo location

--- photoalbum58-android/src/cs213/photoAlbum/util/Utils.java
+++ photoalbum58-android/src/cs213/photoAlbum/util/Utils.java
@@ -68,11 +68,8 @@ public class Utils {
 		int height = image.getHeight();
 		float scaleWidth = ((float) newWidth) / width;
 		float scaleHeight = ((float) newHeight) / height;
-		// create a matrix for the manipulation
 		Matrix matrix = new Matrix();
-		// resize the bit map
 		matrix.postScale(scaleWidth, scaleHeight);
-		// recreate the new Bitmap
 		Bitmap resizedBitmap = Bitmap.createBitmap(image, 0, 0, width, height,
 				matrix, false);
 		return resizedBitmap;
